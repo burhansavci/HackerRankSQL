fa6d8ab3df32da3c485a0907b95c753aed0a119b
Burhan SAVCI <burhansavci@gmail.com>
2020-01-20 21:39:12 +0300

    The PADS question added

---
+++ README.md
@@ -0,0 +1,1 @@
+Each question contains a sample table. You can use these tables if you want to solve the questions yourself.
---
+++ The PADS/The PADS Sample Table.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Occupations(
+ [Name] VARCHAR(50),
+  Occupation VARCHAR(50)
+);
+INSERT INTO Occupations VALUES('Ashley','Professor');
+INSERT INTO Occupations VALUES('Samantha','Actor');
+INSERT INTO Occupations VALUES('Julia','Doctor');
+INSERT INTO Occupations VALUES('Britney','Professor');
+INSERT INTO Occupations VALUES('Maria','Professor');
+INSERT INTO Occupations VALUES('Meera','Professor');
+INSERT INTO Occupations VALUES('Priya','Doctor');
+INSERT INTO Occupations VALUES('Priyanka','Professor');
+INSERT INTO Occupations VALUES('Jennifer','Actor');
+INSERT INTO Occupations VALUES('Ketty','Actor');
+INSERT INTO Occupations VALUES('Belvet','Professor');
+INSERT INTO Occupations VALUES('Naomi','Professor');
+INSERT INTO Occupations VALUES('Jane','Singer');
+INSERT INTO Occupations VALUES('Jenny','Singer');
+INSERT INTO Occupations VALUES('Kristeen','Singer');
+INSERT INTO Occupations VALUES('Christeen','Singer');
+INSERT INTO Occupations VALUES('Eve','Actor');
+INSERT INTO Occupations VALUES('Aamina','Doctor');
+  
+  
+
+  
+  
+  
+  
+  
+  
