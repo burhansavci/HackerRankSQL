9b59045339f6ac9cc9893a039da26d7fda4648e9
Burhan SAVCI <burhansavci@gmail.com>
2020-02-01 16:34:26 +0300

    The Report question added

---
+++ The Report/The Report Sample Table.sql
@@ -0,0 +1,43 @@
+CREATE TABLE Students(
+Id INT,
+[Name] VARCHAR(50),
+Marks INT
+);
+INSERT INTO Students VALUES ('19','Jane','64');
+INSERT INTO Students VALUES ('41','Salma','81');
+INSERT INTO Students VALUES ('76','Amina','89');
+INSERT INTO Students VALUES ('91','Vivek','84');
+INSERT INTO Students VALUES ('21','Julia','96');
+INSERT INTO Students VALUES ('12','Dyana','55');
+INSERT INTO Students VALUES ('13','Jenny','66');
+INSERT INTO Students VALUES ('15','Meera','24');
+INSERT INTO Students VALUES ('16','Priya','76');
+INSERT INTO Students VALUES ('18','Paige','74');
+INSERT INTO Students VALUES ('71','Stuart','99');
+INSERT INTO Students VALUES ('81','Aamina','77');
+INSERT INTO Students VALUES ('51','Amanda','34');
+INSERT INTO Students VALUES ('21','Belvet','78');
+INSERT INTO Students VALUES ('31','Scarlet','80');
+INSERT INTO Students VALUES ('61','Heraldo','94');
+INSERT INTO Students VALUES ('11','Britney','95');
+INSERT INTO Students VALUES ('32','Kristeen','100');
+INSERT INTO Students VALUES ('19','Samantha','87');
+INSERT INTO Students VALUES ('17','Priyanka','77');
+INSERT INTO Students VALUES ('14','Christene','88');
+
+CREATE TABLE Grades(
+Grade INT,
+Min_Mark INT,
+Max_Mark INT
+);
+INSERT INTO Grades VALUES ('1','0','9');
+INSERT INTO Grades VALUES ('2','10','19');
+INSERT INTO Grades VALUES ('3','20','29');
+INSERT INTO Grades VALUES ('4','30','39');
+INSERT INTO Grades VALUES ('5','40','49');
+INSERT INTO Grades VALUES ('6','50','59');
+INSERT INTO Grades VALUES ('7','60','69');
+INSERT INTO Grades VALUES ('8','70','79');
+INSERT INTO Grades VALUES ('9','80','89');
+INSERT INTO Grades VALUES ('10','90','100');
+
